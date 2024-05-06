883d36e12f0a1d9c8c0e806bffdaf81c1deedc39
yatziripineda <131062655+yatziripineda@users.noreply.github.com>
2024-05-06 11:49:14 +0200

    First commit
    Proyecto base

---
+++ Macro/MacroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroApp.swift
+//  Macro
+//
+//  Created by yatziri on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MacroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
