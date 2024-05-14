f4cef68577c22b13c736730844745acd776efb57
yatziripineda <131062655+yatziripineda@users.noreply.github.com>
2024-05-14 10:30:09 +0200

    Home View Beta Start
    Create View :
    -HomeView
    -TopicView
    Add an asset

---
+++ Macro/DiagramModel.swift
@@ -0,0 +1,16 @@
+//
+//  DiagramModel.swift
+//  Macro
+//
+//  Created by yatziri on 14/05/24.
+//
+
+//import Foundation
+//import SwiftData
+//
+//@Model
+//class Recipe {
+//    @Attribute(.unique) var name: String
+//    var summary: String?
+//    var ingredients: [Ingredient]
+//}
---
+++ Macro/HomeView.swift
@@ -0,0 +1,84 @@
+//
+//  HomeView.swift
+//  Macro
+//
+//  Created by yatziri on 13/05/24.
+//
+//
+import SwiftUI
+
+struct HomeView: View {
+    @State private var selectedTopic: Topic? = .all
+    
+    var body: some View {
+        NavigationSplitView {
+            VStack {
+                List(Topic.allCases, selection: $selectedTopic) { topic in
+                    NavigationLink(
+                        destination: TopicDetail(topic: topic),
+                        tag: topic,
+                        selection: $selectedTopic
+                    ) {
+                        HStack {
+                            Image(systemName: "\(topic.localizedIcon)")
+                                .foregroundColor(.blue)
+                            Text(topic.localizedName)
+                        }
+                    }
+                }
+                .listStyle(SidebarListStyle())
+            }
+            .navigationTitle("Title")
+        } 
+    detail: {
+        TopicDetail(topic: selectedTopic)
+        }
+    }
+}
+
+
+
+
+
+enum Topic: Int, Hashable, CaseIterable, Identifiable, Codable {
+    case all
+    case favorites
+
+    var id: Int { rawValue }
+
+    var localizedName: LocalizedStringKey {
+        switch self {
+        case .all:
+            return "All"
+        case .favorites:
+            return "Favorites"
+        }
+    }
+    var localizedIcon: String{
+        switch self {
+        case .all:
+            return "tray"
+        case .favorites:
+            return "heart"
+        }
+    }
+    var localizednavigationTitle: String{
+        switch self {
+        case .all:
+            return "All Subjects"
+        case .favorites:
+            return "Favorites"
+        }
+    }
+}
+
+
+#Preview {
+    HomeView()
+}
+
+
+
+
+
+
